99fd83b12551cb0d54d00f58cde54edade4107d0
Saptarshi Guha <saptarshi.guha@gmail.com>
2016-04-19 19:56:34 -0700

    First Commit for sguha

--- common/01-mozmetrics-setup.py
+++ common/01-mozmetrics-setup.py
@@ -1,4 +1,9 @@
 import os, sys, inspect
 sys.path.insert(0, "/home/hadoop/")
+from pyspark.sql import SQLContext
+from pyspark.sql.types import *
 
+bucket = "telemetry-parquet"
+prefix = "main_summary/v2"
+%time mainpingspq = sqlContext.read.load("s3://{}/{}".format(bucket, prefix), "parquet")
 
--- users/sguha/functions.py
+++ users/sguha/functions.py
@@ -1,9 +1,3 @@
-from pyspark.sql import SQLContext
-from pyspark.sql.types import *
-
-bucket = "telemetry-parquet"
-prefix = "main_summary/v2"
-%time mainpingspq = sqlContext.read.load("s3://{}/{}".format(bucket, prefix), "parquet")
 
 
 
