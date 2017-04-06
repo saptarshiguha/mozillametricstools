ad5b629dc9f9c66dcecccd1ac69147dd6759ae3b
Saptarshi Guha <saptarshi.guha+github@gmail.com>
2017-04-06 14:24:15 -0700

    Update spark.init.step.sh

--- common/spark.init.step.sh
+++ common/spark.init.step.sh
@@ -1,30 +1,25 @@
 
-## the following lines have been commented because i ran them once and because of the 
-## new persistant folders i dont need to run them again.
-## in an ideal world, this could would check for a flag: do i need to run or not and then bail ...
-## but who cares?
+cd ~/
+IS_MASTER=false
+if [ -f /mnt/var/lib/info/instance.json ]
+then
+	IS_MASTER=$(jq .isMaster /mnt/var/lib/info/instance.json)
+fi
+if $IS_MASTER; then
+    ## only runs on master
+  rm -rf /home/hadoop/mozillametricstools   
+  git clone https://github.com/saptarshiguha/mozillametricstools   
+fi 
 
-#cd ~/
-#IS_MASTER=false
-#if [ -f /mnt/var/lib/info/instance.json ]
-#then
-#	IS_MASTER=$(jq .isMaster /mnt/var/lib/info/instance.json)
-#fi
-#if $IS_MASTER; then
-#    ## only runs on master
-#  rm -rf /home/hadoop/mozillametricstools   
-#  git clone https://github.com/saptarshiguha/mozillametricstools   
-#fi 
-
-#echo `ps -o user= -p $$ | awk '{print $1}'`
-#ipython profile create
-## Copy the ipython startup files
-#mkdir -p ~/.ipython/profile_default/startup/ && cp ~/mozillametricstools/01-mozmetrics-setup.py ~/.ipython/profile_default/startup/
+echo `ps -o user= -p $$ | awk '{print $1}'`
+ipython profile create
+# Copy the ipython startup files
+mkdir -p ~/.ipython/profile_default/startup/ && cp ~/mozillametricstools/01-mozmetrics-setup.py ~/.ipython/profile_default/startup/
 
 ## anaconda is now set from this code
 ## https://github.com/mozilla/emr-bootstrap-spark/blob/master/ansible/envs/dev.yml#L6
 ## these i always run since they do not install into a home folder
-/mnt/anaconda2/bin/pip install py4j --upgrade
-/mnt/anaconda2/bin/pip install feather-format
+$HOME/anaconda2/bin/pip install py4j --upgrade
+$HOME/anaconda2/bin/pip install feather-format
 
 
