c31240bf2879f4be6e8c11a7cd4a1c6b9dfee02a
Saptarshi Guha <saptarshi.guha+github@gmail.com>
2017-04-06 13:49:13 -0700

    Update spark.init.step.sh

--- common/spark.init.step.sh
+++ common/spark.init.step.sh
@@ -1,17 +1,17 @@
-cd ~/
-IS_MASTER=false
-if [ -f /mnt/var/lib/info/instance.json ]
-then
-	IS_MASTER=$(jq .isMaster /mnt/var/lib/info/instance.json)
-fi
-if $IS_MASTER; then
-    ## only runs on master
-  rm -rf /home/hadoop/mozillametricstools   
-  git clone https://github.com/saptarshiguha/mozillametricstools   
-fi 
+#cd ~/
+#IS_MASTER=false
+#if [ -f /mnt/var/lib/info/instance.json ]
+#then
+#	IS_MASTER=$(jq .isMaster /mnt/var/lib/info/instance.json)
+#fi
+#if $IS_MASTER; then
+#    ## only runs on master
+#  rm -rf /home/hadoop/mozillametricstools   
+#  git clone https://github.com/saptarshiguha/mozillametricstools   
+#fi 
 
-echo `ps -o user= -p $$ | awk '{print $1}'`
-ipython profile create
+#echo `ps -o user= -p $$ | awk '{print $1}'`
+#ipython profile create
 
 ## anaconda is now set from this code
 ## https://github.com/mozilla/emr-bootstrap-spark/blob/master/ansible/envs/dev.yml#L6
@@ -19,4 +19,4 @@ ipython profile create
 /mnt/anaconda2/bin/pip install feather-format
 ## Copy the ipython startup files
 
-mkdir -p ~/.ipython/profile_default/startup/ && cp ~/mozillametricstools/01-mozmetrics-setup.py ~/.ipython/profile_default/startup/
+#mkdir -p ~/.ipython/profile_default/startup/ && cp ~/mozillametricstools/01-mozmetrics-setup.py ~/.ipython/profile_default/startup/
