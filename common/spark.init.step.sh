2bc57463a6fcc7c3aab73765029b5cad94666a5b
Saptarshi Guha <saptarshi.guha+github@gmail.com>
2017-04-06 14:08:04 -0700

    Update spark.init.step.sh

--- common/spark.init.step.sh
+++ common/spark.init.step.sh
@@ -1,3 +1,9 @@
+
+## the following lines have been commented because i ran them once and because of the 
+## new persistant folders i dont need to run them again.
+## in an ideal world, this could would check for a flag: do i need to run or not and then bail ...
+## but who cares?
+
 #cd ~/
 #IS_MASTER=false
 #if [ -f /mnt/var/lib/info/instance.json ]
@@ -12,11 +18,13 @@
 
 #echo `ps -o user= -p $$ | awk '{print $1}'`
 #ipython profile create
+## Copy the ipython startup files
+#mkdir -p ~/.ipython/profile_default/startup/ && cp ~/mozillametricstools/01-mozmetrics-setup.py ~/.ipython/profile_default/startup/
 
 ## anaconda is now set from this code
 ## https://github.com/mozilla/emr-bootstrap-spark/blob/master/ansible/envs/dev.yml#L6
+## these i always run since they do not install into a home folder
 /mnt/anaconda2/bin/pip install py4j --upgrade
 /mnt/anaconda2/bin/pip install feather-format
-## Copy the ipython startup files
 
-#mkdir -p ~/.ipython/profile_default/startup/ && cp ~/mozillametricstools/01-mozmetrics-setup.py ~/.ipython/profile_default/startup/
+
