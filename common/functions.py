546a0bb89a5691df92af965eb44d3c99aad7aff0
Saptarshi Guha <saptarshi.guha+github@gmail.com>
2017-04-07 12:40:32 -0700

    Update functions.py

--- common/functions.py
+++ common/functions.py
@@ -1,4 +1,5 @@
 import datetime, time
+import pyspark
 
 def latest_longitudinal_path():
     ## use boto,
@@ -58,6 +59,5 @@ def register_udf(sqlc,func, name, return_type):
         as well as registered with the SQLContext. Specify the UDF's return
         type as Spark type.
     """
-    import pyspark
     sqlc.registerFunction(name, func, return_type)
     globals()[name] = pyspark.sql.functions.udf(func, return_type)
